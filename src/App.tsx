7b4c3bdfc70c9f6ce4cfb01ead0b17e5822043e6
JM Dev <juanma261005@gmail.com>
2024-09-22 11:43:35 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./index.css"
+
+function App() {
+
+  return (
+    <>
+      <div className="w-screen h-screen p-4">
+        <p>asdasdasdasdasdad</p>
+      </div>
+    </>
+  )
+}
+
+export default App
