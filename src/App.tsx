3735fc8bdc4eadc4849d648b88c1fcf60d5cb827
JM Dev <juanma261005@gmail.com>
2024-09-26 19:29:10 -0400

    use HashRouter instead BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./screens/login";
 import Home from "./screens/home";
 import Register from "./screens/register";
